bab1864fc2d1cd8a0f338d1e80d6ceec4064d73d
Victor Kemboi <vickemtoo97@gmail.com>
2024-04-03 09:05:04 +0300

    acting on event lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const element = document.getElementById("dodger");
+
+moveDodgerLeft =() =>{
+    let left = element.style.left
+    left = parseInt(left)
+    if (left > 0){
+        element.style.left = `${left - 1}px`
+    }
+}
+moveDodgerRight =() =>{
+    let left = element.style.left
+    left = parseInt(left)
+    if (left < 360){
+        element.style.left = `${left + 1}px`
+    }
+}
+document.addEventListener('keydown', (e) =>{
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+  })
+  
+  document.addEventListener('keydown', (e) => {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
